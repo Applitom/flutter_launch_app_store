6e633f8628f0b26560233a6f81e9d48d9f7d6649
Tomer Shalom <applitom@gmail.com>
2025-02-26 23:08:26 +0200

    FIx for Flutter 3.29.0

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/pessoal/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/pessoal/Workspace/launch_review/example"
+export "FLUTTER_ROOT=/Users/tomer/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tomer/Projects/flutter_launch_app_store/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
